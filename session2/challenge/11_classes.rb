bb28a3ed66ee78e523678f5a332e98e98aeaff65
Denis <denisglb@yahoo.co.uk>
2017-07-17 15:33:11 +0100

    Need to revisit 2:11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -19,14 +19,84 @@
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
 
-num_start = 99
-num_now = num_start
 
-while num_now >0 
-	puts num_now.to_s + "bottles of beer on the wall" + num_now.to_s + "bottles of beer!"
-	num_now = num_now -1 
-	puts "take one down, pass it around" + num_now.to_s + "bottles of beer on the wall"
+class BeerSong
+	attr_accessor :number_of_beers
+	def initialize (number_of_beers)
+		number_of_beers =0 if number_of_beers < 0
+		number_of_beers = 99 if number_of_beers > 99
+	end
+
+	def print_song
+		beers.downto 1 do |i|
+			print_stanza i
+		end
+	end
+
+	def print_stanza(n)
+		if n.zero?
+			String.new 
+		else 
+			puts "#{number_to_words n} #{bottle n} of beer on the wall,"
+			"#{number_to_words n} #{bottle n} of beer,"
+			"Take one down, pass it around,"
+			"#{number_to_words number-1} #{bottle n-1} of beer on the wall"
+ 		end
+	end
+
+	def bottle(n)
+		if n==1 then 'bottle' else 'bottles'end
+	end
+
+
+	def number_to_words(number)
+		numbers = {
+		90 => "Ninety",
+		80 => "Eighty",
+		70 => "Seventy",
+		60 => "Sixty",
+		50 => "Fifty",
+		40 => "Fourty",
+		30 => "Thirty",
+		20 => "Twenty",
+		19 => "Nineteen",
+		18 => "Eighteen",
+		17 => "Seventeen",
+		16 => "Sixteen",
+		15 => "Fifteen",
+		14 => "Fourteen",
+		13 => "Thirteen",
+		12 => "Twelve",
+		11 => "Eleven",
+		10 => "Ten",
+		9 => "Nine",
+		8 => "Eight",
+		7 => "Seven",
+		6 => "Six",
+		5 => "Five",
+		4 => "Four",
+		3 => "Three",
+		2 => "Two",
+		1 => "One",
+		0 => "Zero",
+		}
+		
+		to_return = ""
+		number.each do |num, word| 
+			if number == 0
+				return to_return
+			elsif number.to_s.length == 1 && number/num >0
+				return to_return + "#{word} " if num%number ==0 
+				return to_return + "#{word} " + number_to_words(number%num)
+			elsif number/num > 0
+				return to_return + number_to_words(int/num) + "#{word} " + in_words(int%num)
+			end
+			end
+	end
 end
 
-puts "no more bottles of beer on the wall!"
+
+
+
+
 
